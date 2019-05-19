7cd54f46ecac0f11bf0020f715a14476bcf69ef8
kobayashi-m42 <m42kobayashi@gmail.com>
2019-05-19 17:46:03 +0900

    :sparkles: #24 退会画面を作成

---
+++ app/pages/cancel.vue
@@ -0,0 +1,15 @@
+<template>
+  <Cancel />
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+import Cancel from '@/components/pages/cancel.vue'
+
+@Component({
+  components: {
+    Cancel
+  }
+})
+export default class extends Vue {}
+</script>
