b78d714a16631f48f909fc59a6dbc71ecbcd8831
kobayashi-m42 <m42kobayashi@gmail.com>
2019-08-18 03:15:15 +0900

    :wrench: #59 シングルクォーテーションに修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
     node: true
   },
   extends: [
-    "@nuxtjs/eslint-config-typescript",
+    '@nuxtjs/eslint-config-typescript',
     'plugin:nuxt/recommended',
     'plugin:prettier/recommended',
     'prettier',
